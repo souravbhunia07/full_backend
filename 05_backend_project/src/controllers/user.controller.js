c54b397ceb9d36baad3e226e760c12d838747e51
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2024-06-28 21:05:14 +0530

    add subscription model

--- 05_backend_project/src/controllers/user.controller.js
+++ 05_backend_project/src/controllers/user.controller.js
@@ -206,4 +206,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
